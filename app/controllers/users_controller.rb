e806f15a75c6a89699a81fcf79e5d159afabb5c9
ozPop <ozziepopovas@gmail.com>
2016-11-17 08:57:57 -0500

    add css files, update user show, disable turbolinks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
 
   def show
-    # url = 'https://api.github.com/users/' + current_user.username
-    # @resp = Faraday.get url do |req|
-    #     req.params['access_token'] = current_user.token
-    #   end
-    #   body_hash = JSON.parse(@resp.body)
-    #   current_user.update_attr(body_hash)
-    #   render 'show'
+    url = 'https://api.github.com/users/' + current_user.username
+    @resp = Faraday.get url do |req|
+        req.params['access_token'] = current_user.token
+      end
+      body_hash = JSON.parse(@resp.body)
+      current_user.update_attr(body_hash)
+      render 'show'
   end
 
 end
