85354eedf6cd5cfbf29bcf7813ab6b22f394bb9c
Esmery Corniel <esmerycornielle@gmail.com>
2016-11-17 11:37:52 -0500

    update user show, user.css, fullpage.js

--- app/assets/javascripts/fullpage-custom.js
+++ app/assets/javascripts/fullpage-custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   if ( $( 'html' ).hasClass( 'fp-enabled' ) !== true ) {
     $('#fullpage').fullpage({
-      sectionsColor: ['#1bbc9b', '#4BBFC3'],
+      sectionsColor: ['#02C39A', '#00A896'],
       anchors: ['firstPage', 'secondPage'],
       menu: '#menu',
       scrollingSpeed: 1000
