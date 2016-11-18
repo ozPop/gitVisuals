e8d375ccda2a42d2eeccf8a3a7bd120d1ade33c0
Kelene Chung <simoy93@gmail.com>
2016-11-18 16:29:27 -0500

    restyle the background color for all pages

--- app/assets/javascripts/fullpage-custom.js
+++ app/assets/javascripts/fullpage-custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   if ( $( 'html' ).hasClass( 'fp-enabled' ) !== true ) {
     $('#fullpage').fullpage({
-      sectionsColor: ['#02C39A', '#00A896'],
+      sectionsColor: ['#e0dfdc', '#e2e0df'],
       anchors: ['firstPage', 'secondPage'],
       menu: '#menu',
       scrollingSpeed: 1000
