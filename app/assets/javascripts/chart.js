fcb866f284b23061e3c90efd170686454db5f927
Natalisp <nathalipat@gmail.com>
2016-11-19 12:07:34 -0500

    add pie chart

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -1,5 +1,7 @@
+
 function renderCharts(repos) {
 
+// BAR CHART
   var months = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
   var reposCount =  months.map(function(month){
       return getRepoCount(month, repos);
@@ -12,13 +14,8 @@ function renderCharts(repos) {
   }).length;
   }
 
-Chart.defaults.global
-
-  var ctx = document.getElementById("myChart");
+  var ctx = document.getElementById("barChart");
   var myChart = new Chart(ctx, {
-    title:{
-       text: "Index Labels Customizations"
-      },
     type: 'bar',
     data: {
         labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
@@ -31,20 +28,80 @@ Chart.defaults.global
         }]
     },
     options: {
+      tooltips: {
+        titleFontSize: 15
+      },
         scales: {
             yAxes: [{
                 ticks: {
                     beginAtZero:true,
                     fontSize: 20
                 }
-            }],
+              }],
             xAxes: [{
             ticks: {
                 fontSize: 20
-            }
+              },
+            barPercentage: 1
         }]
-        }
+      },
+      legend: {
+          position: 'top',
+          labels: {
+            fontSize: 20
+          }
+      }
     }
 });
+
+//pieChart
+var pie = document.getElementById("pieChart");
+var reposLanguages = repoLanguages(repos);
+var l = languageStats(reposLanguages);
+
+var myPieChart = new Chart(pie, {
+    type: 'pie',
+    data: {
+        labels: ["Ruby", "HTML", "JavaScript", "CSS"],
+        datasets: [{
+          backgroundColor: [
+            "#ca4444",
+            "#3498db",
+            "#95a5a6",
+            "#9b59b6"
+          ],
+          data: [
+            l['Ruby'],
+            l['HTML'],
+            l['JavaScript'],
+            l['CSS']
+          ]
+        }]
+      },
+  options: {
+        tooltips: {
+          titleFontSize: 20,
+          bodyFontSize: 20
+        },
+         responsive: true,
+         legend: {
+             position: 'top',
+             labels: {
+               fontSize: 30
+             }
+         },
+         scale: {
+           ticks: {
+             beginAtZero: true
+           },
+           reverse: false
+         },
+         animation: {
+             animateRotate: false,
+             animateScale: true
+         }
+     }
+});
+
 
 }
