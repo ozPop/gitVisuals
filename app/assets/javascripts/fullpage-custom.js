436f8a410502baf044ea8b329d65eeca61488456
ozPop <ozziepopovas@gmail.com>
2016-11-20 18:49:15 -0500

    add #restartFullpage

--- app/assets/javascripts/fullpage-custom.js
+++ app/assets/javascripts/fullpage-custom.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
   if ( $( 'html' ).hasClass( 'fp-enabled' ) !== true ) {
     $('#fullpage').fullpage({
-      sectionsColor: ['#1bbc9b', '#4BBFC3'],
-      anchors: ['firstPage', 'secondPage'],
+      sectionsColor: ['#1bbc9b', '#4BBFC3', '#4B8BC3'],
+      anchors: ['firstPage', 'secondPage', 'thirdPage'],
       menu: '#menu',
       scrollingSpeed: 1000
     });
   }
 });
+
+function restartFullpage() {
+  $.fn.fullpage.destroy('all');
+  if ( $( 'html' ).hasClass( 'fp-enabled' ) !== true ) {
+    $('#fullpage').fullpage({
+      sectionsColor: ['#1bbc9b', '#4BBFC3', '#4B8BC3'],
+      anchors: ['firstPage', 'secondPage', 'thirdPage'],
+      menu: '#menu',
+      scrollingSpeed: 1000
+    });
+  }
+}
